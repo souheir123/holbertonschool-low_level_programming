5fcae1df9fcab4e7b226f5cdfd6584da8fe9edf9
souheir123 <11497@studentsholberton.com>
2025-06-21 08:51:11 +0000

    add all change

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        char low;
+    char low;
 
-        for (low = 'a'; low <= 'z'; low++)
-                putchar(low);
-                putchar('\n');
+    for (low = 'a'; low <= 'z'; low++)
+    {
+        putchar(low);
+    }
 
-        return (0);
+    putchar('\n');
+
+    return (0);
 }
