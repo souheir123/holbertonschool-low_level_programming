e1ce27c63a980a1fa0f07ac01f3718b6c234ea39
souheir123 <11497@studentsholberton.com>
2025-06-22 14:11:36 +0000

    add 7-print_last_digit.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,19 +1,8 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int r;
+/* Tes prototypes de fonctions ici */
+int _putchar(char c);
+int print_last_digit(int n);
 
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-
-    return (0);
-}
+#endif /* MAIN_H */
