9e7ddbc551f6e00355667153f0f1a2aefce87787
souheir123 <11497@studentsholberton.com>
2025-06-24 09:50:39 +0000

    add 5-more_numbers.c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 void print_numbers(void);
-void print_most_numbers(void); /* ← ajout ici */
+void print_most_numbers(void);
+void more_numbers(void);
 
 #endif /* MAIN_H */
