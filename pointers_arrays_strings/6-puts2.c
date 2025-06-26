1e3adecb9d3d8afcbaa704fc463229b12d7bcd47
root <you@example.com>
2025-06-26 12:19:39 +0000

    add all change

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,15 @@
  */
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-        {
-            _putchar(str[i]);
-        }
-        i++;
-    }
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
 }
